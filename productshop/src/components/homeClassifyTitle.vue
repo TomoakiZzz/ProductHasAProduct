57cf7b12d2c941c625604af049cf75b742717265
wzh <972036796@qq.com>
2019-08-13 17:03:09 +0800

    首页列表渲染

---
+++ productshop/src/components/homeClassifyTitle.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h3></h3>
+        <p><span></span><span><i class=""></i></span></p>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
--- productshop/src/store/modules/home.js
+++ productshop/src/store/modules/home.js
@@ -1,18 +1,23 @@
 import {getHomeData,getCategoryList} from "../../service/index"
 const state={
-    bannerList:[]
+    //首页数据
+    homeListData:[]
 }
 const mutations={
+    //首页分配数据
     homeAllocationData(state,payload){
-
+        console.log(payload)
+        state.homeListData = payload
     }
 }
 const actions = {
+    //获取首页的数据
     async getHomeListData({commit},payload){
         let data = await getHomeData(123)
 
         // let data = await getCategoryList(123)
         console.log(data)
+        commit("homeAllocationData",data.result)
     },
     async getCategoryList(){
         
