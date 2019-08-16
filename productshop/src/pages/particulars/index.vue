ea0bbcb8f35c71abaae3ede7b90e70bf02bbe0e8
myl <1244282340@qq.com>
2019-08-16 08:56:01 +0800

    tab页面

--- productshop/src/pages/particulars/index.vue
+++ productshop/src/pages/particulars/index.vue
@@ -44,7 +44,11 @@
       </div>
       <div class="particulBottom">
         <div class="bottomNr">
-          <div class="nr" v-for="(ite,index) in classifyProductData" :key="index">
+          <div class="nr" 
+          v-for="(ite,index) in classifyProductData" 
+          :key="index"
+          @click="commodity(ite.pid)"
+          >
             <p class="nrTop">
               <img :src="ite.mainImgUrl" alt class="particulars_img" />
             </p>
@@ -99,6 +103,16 @@ export default {
     ...mapMutations({
       updateInd: "page/updateInd"
     }),
+    commodity(id){
+      wx.navigateTo({
+        url: '/pages/goodsDetail/main?id='+id,
+        success: (result)=>{
+          
+        },
+        fail: ()=>{},
+        complete: ()=>{}
+      });
+    },
     handTopTab(index) {
       this.updateInd(index);
       this.classifyProducts({
