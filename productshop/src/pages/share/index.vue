bb4cc4eb54b4cd61e8b16c9f49156036a6ebd3bd
wzh <972036796@qq.com>
2019-08-16 11:34:08 +0800

    专题页面

--- productshop/src/pages/share/index.vue
+++ productshop/src/pages/share/index.vue
@@ -8,30 +8,30 @@
 <template>
   <div class="share_wrap">
     <div class="share_content">
-        <canvas style="width: 100%;height: 100%;" canvas-id="shareFrends"></canvas>
+      <canvas style="width: 100%;height: 100%;" canvas-id="shareFrends"></canvas>
     </div>
     <div class="share_btn">
-        <div class="share_btn_item">
-            <img src="../../../static/images/复制文案.svg" alt="">
-            <span>复制文案</span>
-        </div>
-        <div class="share_btn_item">
-            <img src="../../../static/images/分享微信.svg" alt="">
-            <span>微信好友</span>
-        </div>
-        <div class="share_btn_item">
-            <img src="../../../static/images/朋友圈 copy 2.svg" alt="">
-            <span>朋友圈</span>
-        </div>
-        <div class="share_btn_item" @click="saveImg">
-            <img src="../../../static/images/保存图片 copy 2.svg" alt="">
-            <span>保存到相册</span>
-        </div>
+      <div class="share_btn_item">
+        <img src="../../../static/images/复制文案.svg" alt>
+        <span>复制文案</span>
+      </div>
+      <div class="share_btn_item">
+        <img src="../../../static/images/分享微信.svg" alt>
+        <span>微信好友</span>
+      </div>
+      <div class="share_btn_item">
+        <img src="../../../static/images/朋友圈 copy 2.svg" alt>
+        <span>朋友圈</span>
+      </div>
+      <div class="share_btn_item" @click="saveImg">
+        <img src="../../../static/images/保存图片 copy 2.svg" alt>
+        <span>保存到相册</span>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import { mapState, mapActions, mapMutations } from "vuex"
+import { mapState, mapActions, mapMutations } from "vuex";
 
 export default {
   props: {},
@@ -40,85 +40,84 @@ export default {
     return {
       userInfo: {},
       shareImg: "",
-      flag: ''
+      flag: ""
     };
   },
-  onLoad(options){
-      let that = this;
-      this.getDataFn({pid:Number(options.id)})   //获取详情
-      wx.getStorage({
-      key: 'user',
-      success (res) {
-        that.userInfo = res.data
+  onLoad(options) {
+    let that = this;
+    this.getDataFn({ pid: Number(options.id) }); //获取详情
+    wx.getStorage({
+      key: "user",
+      success(res) {
+        that.userInfo = res.data;
       }
-    })
+    });
   },
   computed: {
     ...mapState({
-      goodsItem: state => state.goods.goodsItem,  //产品详情
+      goodsItem: state => state.goods.goodsItem //产品详情
     })
   },
   methods: {
     ...mapActions({
-      getDataFn:'goods/getDataFn'
+      getDataFn: "goods/getDataFn"
     }),
     saveImg() {
-    let that = this;
-    // 获取用户是否开启用户授权相册
-    wx.getSetting({
-      success(res) {
-        // 如果没有则获取授权
-        if (!res.authSetting['scope.writePhotosAlbum']) {
-          wx.authorize({
-            scope: 'scope.writePhotosAlbum',
-            success() {
-              wx.saveImageToPhotosAlbum({
-                filePath: that.shareImg,
-                success() {
-                  wx.showToast({
-                    title: '保存成功'
-                  })
-                },
-                fail() {
-                  wx.showToast({
-                    title: '保存失败',
-                    icon: 'none'
-                  })
-                }
-              })
-            },
-            fail() {
-            // 如果用户拒绝过或没有授权，则再次打开授权窗口
-            //（ps：微信api又改了现在只能通过button才能打开授权设置，以前通过openSet就可打开，下面有打开授权的button弹窗代码）
-              
-            }
-          })
-        } else {
-          // 有则直接保存
-          wx.saveImageToPhotosAlbum({
-            filePath: that.shareImg,
-            success() {
-              wx.showToast({
-                title: '保存成功'
-              })
-            },
-            fail() {
-              wx.showToast({
-                title: '保存失败',
-                icon: 'none'
-              })
-            }
-          })
+      let that = this;
+      // 获取用户是否开启用户授权相册
+      wx.getSetting({
+        success(res) {
+          // 如果没有则获取授权
+          if (!res.authSetting["scope.writePhotosAlbum"]) {
+            wx.authorize({
+              scope: "scope.writePhotosAlbum",
+              success() {
+                wx.saveImageToPhotosAlbum({
+                  filePath: that.shareImg,
+                  success() {
+                    wx.showToast({
+                      title: "保存成功"
+                    });
+                  },
+                  fail() {
+                    wx.showToast({
+                      title: "保存失败",
+                      icon: "none"
+                    });
+                  }
+                });
+              },
+              fail() {
+                // 如果用户拒绝过或没有授权，则再次打开授权窗口
+                //（ps：微信api又改了现在只能通过button才能打开授权设置，以前通过openSet就可打开，下面有打开授权的button弹窗代码）
+              }
+            });
+          } else {
+            // 有则直接保存
+            wx.saveImageToPhotosAlbum({
+              filePath: that.shareImg,
+              success() {
+                wx.showToast({
+                  title: "保存成功"
+                });
+              },
+              fail() {
+                wx.showToast({
+                  title: "保存失败",
+                  icon: "none"
+                });
+              }
+            });
+          }
         }
-      }
-    })
-  },
+      });
+    }
   },
   mounted() {
-    let that=this;
-     //绘图上下文
-    let ctx = wx.createCanvasContext('shareFrends'); 
-    console.log(this.userInfo)
+    let that = this;
+    //绘图上下文
+    let ctx = wx.createCanvasContext("shareFrends");
+    console.log(this.userInfo);
     //绘制头像
     // 绘制用户名和头像
     ctx.save();
@@ -126,88 +125,119 @@ export default {
     // ctx.fillStyle = "#fff";
     // ctx.clip();
     // ctx.fill();
-    ctx.drawImage(this.userInfo.avatarUrl,0, 0, 120, 120, 15, 30, 40, 40);
+    ctx.drawImage(this.userInfo.avatarUrl, 0, 0, 120, 120, 15, 30, 40, 40);
     ctx.save();
     ctx.setFontSize(12);
-    ctx.fillText(`${this.userInfo.nickName}分享给你一个商品`, 67, 40)
-    ctx.fillText(`邀请码: dasbdabdbaj`, 67, 60)
+    ctx.fillText(`${this.userInfo.nickName}分享给你一个商品`, 67, 40);
+    ctx.fillText(`邀请码: dasbdabdbaj`, 67, 60);
     ctx.restore();
     // 绘制商品大图和标题
     ctx.save();
-     ctx.setFontSize(12);
-     let row = 0, pos = 0;
-      for (let i=0; i<this.goodsItem.title.length; i++){
-        let str = this.goodsItem.title.slice(pos, i);
-        if (ctx.measureText(str).width > 266 && ctx.measureText(str).width < 280){
-          ctx.fillText(this.goodsItem.title.slice(pos, i), 20, 360+15*row);
-          row++;
-          pos = i;
-        }
+    ctx.setFontSize(12);
+    let row = 0,
+      pos = 0;
+    for (let i = 0; i < this.goodsItem.title.length; i++) {
+      let str = this.goodsItem.title.slice(pos, i);
+      if (
+        ctx.measureText(str).width > 266 &&
+        ctx.measureText(str).width < 280
+      ) {
+        ctx.fillText(this.goodsItem.title.slice(pos, i), 20, 360 + 15 * row);
+        row++;
+        pos = i;
       }
-      if (pos < this.goodsItem.title.length){
-        ctx.fillText(this.goodsItem.title.slice(pos, this.goodsItem.title.length), 20, 360+15*row);
-      }
+    }
+    if (pos < this.goodsItem.title.length) {
+      ctx.fillText(
+        this.goodsItem.title.slice(pos, this.goodsItem.title.length),
+        20,
+        360 + 15 * row
+      );
+    }
     wx.getImageInfo({
-        src: this.goodsItem.mainImgUrl, //图片的路径，可以是相对路径，临时文件路径，存储文件路径，网络图片路径,
-        complete: res => {
-          console.log('res...', res);
-          let rx, ry, rw, rh;
-          if (res.width/res.height > 220/260){
-            ry = 0;
-            rh = res.height;
-            rw = res.height/260*220;
-            rx = (res.width - rw)/2;
-          }else{
-            rx = 0;
-            rw = res.width;
-            rh = res.height/220*260;
-            ry = (res.height - rh)/2;
-          }
-          ctx.drawImage(this.goodsItem.mainImgUrl, 0, 0, res.width, res.height, 50, 80, 220, 260);
-          ctx.draw(true, ()=>{
-            // 生成图片
-            wx.canvasToTempFilePath({
-              x: 0,
-              y: 0,
-             
-              canvasId: 'shareFrends',
-              quality: 1,
-              fileType: 'jpg',
-              complete: res=>{
-                console.log('tmpFile...', res);
-                this.shareImg = res.tempFilePath;
-                // 预览一下
-                // wx.previewImage({
-                //   urls: [res.tempFilePath] //需要预览的图片链接列表,
-                // });
-                // 保存到本地
-              }
-            })
-          });
+      src: this.goodsItem.mainImgUrl, //图片的路径，可以是相对路径，临时文件路径，存储文件路径，网络图片路径,
+      complete: res => {
+        console.log("res...", res);
+        let rx, ry, rw, rh;
+        if (res.width / res.height > 220 / 260) {
+          ry = 0;
+          rh = res.height;
+          rw = res.height / 260 * 220;
+          rx = (res.width - rw) / 2;
+        } else {
+          rx = 0;
+          rw = res.width;
+          rh = res.height / 220 * 260;
+          ry = (res.height - rh) / 2;
         }
-      });
-       // 计算多行文本，自动换行
-     
-       // 绘制二维码
-      ctx.save();
-      ctx.setFontSize(15);
-      ctx.fillText('长按识别二维码访问', 20, 480);
-      ctx.restore();
-      ctx.drawImage('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565922951830&di=1c2dbe3068da2172cc58b2fa4c71d504&imgtype=0&src=http%3A%2F%2Fthumb12.jfcdns.com%2F2018-06%2Fbce5b34974538f2e.jpeg', 0, 0, 2160, 2160, 240, 420, 80, 80);
-      // 绘制商品价格
-      // ctx.save();
-      ctx.setFontSize(10);
-      ctx.setFillStyle('#ff0000');
-      ctx.fillText('￥', 20, 450);
-      ctx.setFontSize(18);
-      ctx.fillText(this.goodsItem.salesPrice, 30, 450);
-      ctx.setFontSize(10);
-      ctx.setFillStyle('#999');
-      // info.result.marketPrice = '88888.888888';
-      ctx.fillText(`￥${this.goodsItem.marketPrice}`, 155-ctx.measureText(`￥${this.goodsItem.marketPrice}`).width, 450);
-      ctx.restore();
-      ctx.draw();
-   
+        ctx.drawImage(
+          this.goodsItem.mainImgUrl,
+          0,
+          0,
+          res.width,
+          res.height,
+          50,
+          80,
+          220,
+          260
+        );
+        ctx.draw(true, () => {
+          // 生成图片
+          wx.canvasToTempFilePath({
+            x: 0,
+            y: 0,
+
+            canvasId: "shareFrends",
+            quality: 1,
+            fileType: "jpg",
+            complete: res => {
+              console.log("tmpFile...", res);
+              this.shareImg = res.tempFilePath;
+              // 预览一下
+              // wx.previewImage({
+              //   urls: [res.tempFilePath] //需要预览的图片链接列表,
+              // });
+              // 保存到本地
+            }
+          });
+        });
+      }
+    });
+    // 计算多行文本，自动换行
+
+    // 绘制二维码
+    ctx.save();
+    ctx.setFontSize(15);
+    ctx.fillText("长按识别二维码访问", 20, 480);
+    ctx.restore();
+    ctx.drawImage(
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565922951830&di=1c2dbe3068da2172cc58b2fa4c71d504&imgtype=0&src=http%3A%2F%2Fthumb12.jfcdns.com%2F2018-06%2Fbce5b34974538f2e.jpeg",
+      0,
+      0,
+      2160,
+      2160,
+      240,
+      420,
+      80,
+      80
+    );
+    // 绘制商品价格
+    // ctx.save();
+    ctx.setFontSize(10);
+    ctx.setFillStyle("#ff0000");
+    ctx.fillText("￥", 20, 450);
+    ctx.setFontSize(18);
+    ctx.fillText(this.goodsItem.salesPrice, 30, 450);
+    ctx.setFontSize(10);
+    ctx.setFillStyle("#999");
+    // info.result.marketPrice = '88888.888888';
+    ctx.fillText(
+      `￥${this.goodsItem.marketPrice}`,
+      155 - ctx.measureText(`￥${this.goodsItem.marketPrice}`).width,
+      450
+    );
+    ctx.restore();
+    ctx.draw();
   }
 };
 </script>
@@ -241,12 +271,12 @@ export default {
     margin-bottom: 10rpx;
   }
 }
-.userBtn{
+.userBtn {
   position: fixed;
-  top:0;
-  left:0;
-  width:100%;
-  height:100%;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   z-index: 999;
   opacity: 0;
 }
--- productshop/src/pages/specialPage/index.vue
+++ productshop/src/pages/specialPage/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <div></div>
+  <div class="wrap" v-if="specialData && specialData.anchors">
+    <div class="specialBan">
+      <div class="specialBanPic">
+        <img :src="specialData.specialImg" alt>
+      </div>
+      <div class="specialTitle">
+        <span class="active" v-for="item in specialData.anchors" :key="item.said">{{item.anchorName}}</span>
+      </div>
+    </div>
+
+    <div class="specialShopWrap">
+      <div class="shopTitle">
+        <span></span>
+        <b></b>
+        <i>{{specialData.anchors[0].anchorDesc}}</i>
+        <b></b>
+        <span></span>
+      </div>
+
+      <div class="shopList">
+        <dl class="shopItem" v-for="item in specialData.anchors[0].products" :key="item.cid">
+          <dt class="shopPic">
+            <img :src="item.mainImgUrl" alt>
+          </dt>
+          <dd class="shopContext">
+            <p class="shopName">{{item.title}}</p>
+            <p class="shopPirce">
+              <span>￥</span>
+              <span>{{item.salesPrice}}</span>
+            </p>
+            <p class="discounts">
+              <span>{{item.earnMoney}}</span>
+            </p>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { mapActions, mapState } from "vuex";
@@ -20,11 +57,152 @@ export default {
     })
   },
   onLoad(options) {
-    console.log(options);
+    console.log(options, "optionsId");
     this.getSpecial({ siid: options.businessId });
   },
   mounted() {}
 };
 </script>
-<style scoped lang="">
+<style scoped lang="scss">
+.wrap {
+  width: 100%;
+  background: #f3f7f7;
+  .specialBan {
+    width: 100%;
+    .specialBanPic {
+      width: 100%;
+      img {
+        width: 100%;
+        height: 300rpx;
+      }
+    }
+  }
+  .specialTitle {
+    width: 100%;
+    height: 92rpx;
+    line-height: 92rpx;
+    display: flex;
+    white-space: nowrap;
+    border-bottom: 2rpx solid #ececec;
+    justify-content: space-between;
+    background: #fff;
+    span {
+      height: 92rpx;
+      margin: 0 20rpx;
+      // flex: 1;
+      display: inline;
+    }
+    .active {
+      color: red;
+      border-bottom: 4rpx solid red;
+    }
+  }
+  .specialShopWrap {
+    width: 100%;
+    .shopTitle {
+      width: 100%;
+      display: flex;
+      height: 150rpx;
+      box-sizing: border-box;
+      justify-content: center;
+      align-items: center;
+      span {
+        width: 68rpx;
+        height: 2rpx;
+        background: #484848;
+      }
+      b {
+        width: 14rpx;
+        height: 14rpx;
+        background: #484848;
+        transform: rotate(45deg);
+      }
+      i {
+        font-size: 28rpx;
+        font-family: PingFangSC-Medium;
+        font-weight: 500;
+        color: #484848;
+        font-style: normal;
+        padding: 0 60rpx;
+      }
+    }
+  }
+}
+.shopList {
+  width: 100%;
+  padding: 8rpx;
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  dl.shopItem {
+    width: 236rpx;
+    height: 484rpx;
+    background: #fff;
+    border-radius: 10rpx;
+    overflow: hidden;
+    margin: 10rpx 0 0 8rpx;
+    box-sizing: border-box;
+    .shopPic {
+      width: 100%;
+      height: 264rpx;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .shopContext {
+      .shopName {
+        box-sizing: border-box;
+        padding: 10rpx;
+        width: 254rpx;
+        height: 80rpx;
+        font-size: 24rpx;
+        font-family: PingFangSC-Regular;
+        font-weight: 400;
+        color: #323a45;
+        line-height: 34rpx;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+      .shopPirce {
+        width: 100%;
+        display: flex;
+        margin-top: 10rpx;
+        span:first-of-type {
+          width: 18rpx;
+          height: 32rpx;
+          font-size: 28rpx;
+          font-family: PingFangSC-Medium;
+          font-weight: 500;
+          color: #fc5d7b;
+          line-height: 32rpx;
+          margin-top: 20rpx;
+        }
+        span:last-of-type {
+          height: 64rpx;
+          font-size: 36rpx;
+          font-family: DINAlternate-Bold;
+          font-weight: 700;
+          color: #fc5d7b;
+          line-height: 62rpx;
+          margin-top: 2rpx;
+          width: 38rpx;
+          margin-left: 8rpx;
+        }
+      }
+      .discounts {
+        height: 24rpx;
+        font-size: 22rpx;
+        font-family: PingFangSC-Regular;
+        font-weight: 400;
+        color: #999da2;
+        line-height: 24rpx;
+        color: #fc5d7b;
+      }
+    }
+  }
+}
 </style>
